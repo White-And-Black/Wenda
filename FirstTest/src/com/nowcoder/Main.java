664979193a9cc462f956b8f1fca85aa202ed109c
it'sme <1021424695@qq.com>
2016-07-15 22:38:22 +0800

    FirstText1

---
+++ FirstTest/src/com/nowcoder/Main.java
@@ -0,0 +1,10 @@
+package com.nowcoder;
+
+/**
+ * Created by white and black on 2016/7/15.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.print("hello world!");
+    }
+}
