98d4a8c344e23a04b69801eb5479cf82b4618072
it'sme <1021424695@qq.com>
2016-07-16 08:58:29 +0800

    FirstText3

--- FirstTest/src/com/nowcoder/Main.java
+++ FirstTest/src/com/nowcoder/Main.java
@@ -1,11 +1,17 @@
 package com.nowcoder;
 
+import java.util.Objects;
+
 /**
  * Created by white and black on 2016/7/15.
  */
 public class Main {
+    public static void print(int index, Object object){
+        System.out.println(String.format("(%d):%s",index,object.toString()));
+    }
     public static void main(String[] args){
         System.out.println("hello world!");
         System.out.println("i have some change");
+        print(1,"helloworld");
     }
 }
